2b5582e4ec8b903527188e214e88f74861938e5d
roberto <roberto@golgi.local>
2016-02-28 15:17:16 +0100

    add db configuration file example

--- php/base.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-$dbhost = "localhost"; // this will ususally be 'localhost', but can sometimes differ
-$dbuser = "root"; // the username that you created, or were given, to access your database
-$dbpass = "beo8hkii"; // the password that you created, or were given, to access your database
-$dblogin = "MyUsers";	// database containing the users list
-?>
