7462a7206f5050d137dd7c637bcaac58cfca4c5c
roberto <roberto@golgi.local>
2016-07-13 11:39:53 +0200

    added 'modified' key/value during creation of info.json file

--- js/atlasMaker.js
+++ js/atlasMaker.js
@@ -2,7 +2,7 @@ var AtlasMakerWidget = {
 	//========================================================================================
 	// Globals
 	//========================================================================================
-	debug:			3,
+	debug:			1,
 	container:		null,	// Element where atlasMaker lives
 	brain_offcn:	null,
 	brain_offtx:	null,
