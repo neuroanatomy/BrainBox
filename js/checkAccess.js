6a1005aa29355fe661d077c19a9acc7844c09b6b
roberto <roberto@golgi.local>
2016-10-09 11:39:43 +0200

    handle 'anyone' user

--- js/checkAccess.js
+++ js/checkAccess.js
@@ -6,20 +6,36 @@ var toMRI = function(mri, projects, user, access) {
     for(p in projects) {
         if(projects[p].files.list.indexOf(mri.source)>=0) {
             console.log(mri.source,user,access,projects[p].files.list.indexOf(mri.source));
-            var publicLevel = accessLevels.indexOf(projects[p].files.access.public);
+            
+            // find 'anyone' user
+            var i, anyone, found = false;
+            for(i=0;i<projects[p].collaborators.list.length;i++) {
+                if(projects[p].collaborators.list[i].userID === 'anyone') {
+                    anyone=projects[p].collaborators.list[i];
+                    found = true;
+                    break;
+                }
+            }
+            if(found === false) {
+                console.log("ERROR: require 'anyone' user not found");
+            }
+            
+            // check file access level of 'anyone'
+            var publicLevel = accessLevels.indexOf(anyone.access.files);
             var c, collaborators;
         
             // check if user has owner access
-            if(user === projects[p].files.access.owner) {
+            if(user === projects[p].owner) {
                 continue;
             }
         
             // check if user has collaborator access
             var accessOk = false;
-            collaborators=projects[p].files.access.whitelist;
+            // collaborators=projects[p].files.access.whitelist;
+            collaborators=projects[p].collaborators.list;
             for(c in collaborators) {
                 if(collaborators[c].userId === user) {
-                    var collaboratorAccessLevel = accessLevels.indexOf(collaborators[c].access);
+                    var collaboratorAccessLevel = accessLevels.indexOf(collaborators[c].access.files);
                     if(requestedLevel>collaboratorAccessLevel) {
                         console.log("Collaborator access refused from project",projects[p].shortname);
                         return false;
@@ -46,19 +62,36 @@ var toMRI = function(mri, projects, user, access) {
 var toProject = function(project, user, access) {
     var p, requestedLevel = accessLevels.indexOf(access);
 
-    var publicLevel = accessLevels.indexOf(project.files.access.public);
+    // find 'anyone' user
+    var i, anyone, found = false;
+    console.log("project",JSON.stringify(project));
+    console.log("list",project.collaborators.list);
+    for(i=0;i<project.collaborators.list.length;i++) {
+        if(project.collaborators.list[i].userID === 'anyone') {
+            anyone=project.collaborators.list[i];
+            found = true;
+            break;
+        }
+    }
+    if(found === false) {
+        console.log("ERROR: require 'anyone' user not found");
+    }
+
+    // find file access level of 'anyone'
+    var publicLevel = accessLevels.indexOf(anyone.access.files);
     var c, collaborators;
 
     // check if user has owner access
-    if(user === project.files.access.owner) {
+    if(user === project.owner) {
         return true;
     }
 
     // check if user has collaborator access
-    collaborators=project.files.access.whitelist;
+    //collaborators=project.files.access.whitelist;
+    collaborators=project.collaborators.list;
     for(c in collaborators) {
         if(collaborators[c].userId === user) {
-            var collaboratorAccessLevel = accessLevels.indexOf(collaborators[c].access);
+            var collaboratorAccessLevel = accessLevels.indexOf(collaborators[c].access.files);
             if(requestedLevel>collaboratorAccessLevel) {
                 console.log("Collaborator access refused to project",project.shortname);
                 return false;
