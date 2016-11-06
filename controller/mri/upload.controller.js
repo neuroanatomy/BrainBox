4eddd59432543d680b61f0734ae8a0c545f0fb98
roberto <roberto@golgi.local>
2016-11-07 00:09:17 +0100

    create directory for annotation if it doesn't exist

--- controller/mri/upload.controller.js
+++ controller/mri/upload.controller.js
@@ -97,14 +97,11 @@ var upload = function(req, res) {
     console.log("atlasProject:", atlasProject);
     console.log("atlasLabelSet:", atlasLabelSet);
     console.log("files:", files);
-
-    // Check that there is not an atlas with this atlasName and atlasProject
-    // ==> something like this should be empty: db.get("mri").find({"mri.atlas.$.name":atlasName, "mri.atlas.$.project":atlasProject})
-    
-    // move atlas file to final directory
+        
+    // create final filename
     var ext;
     var filename;
-    var path;
+    var dir, path;
     if(/.nii.gz$/.test(files[0].originalname))
         ext=".nii.gz";
     else
@@ -114,52 +111,83 @@ var upload = function(req, res) {
         return res.json({error:"Atlas encoding neither .nii.gz nor .mgz"}).status(400).end();
     }
     filename=Math.random().toString(36).slice(2)+ext;
-    path = req.dirname + "/public" + mri.url + filename;
-    fs.rename(req.dirname  + "/" + files[0].path, path);
+    
+    // check if directory exists (it may not exist if a volume annotation is being uploaded
+    // for an mri that has only a db entry but has not yet been accessed)    
+    dir = req.dirname + "/public" + mri.url;
+    if(!fs.existsSync(dir)) {
+        // directory does not exist, create it
+        console.log("> mri directory did not exist, create it");
+        fs.mkdirSync(dir, '0777');
+    }
+
+    // move tmp atlas file to final location
+    path = dir + filename;
+    try {
+        fs.renameSync(req.dirname  + "/" + files[0].path, path);
+    } catch(err) {
+        console.log("ERROR rename failed:",err);
+        return res.json({error:"cannot upload volume annotation"}).status(400).end();
+    };
 
     // Check that the dimensions of the atlas are the same as its parent mri
+    console.log("> load parent mri");
     atlasMakerServer.loadMRI(path)
-        .then(function(atlas){
-            console.log("atlas.dim: ",atlas.dim);
-            console.log("mri.dim: ",mri.dim);
+    .then(function(atlas){
+        console.log("atlas.dim: ",atlas.dim);
+        console.log("mri.dim: ",mri.dim);
 
-            // check volume dimensions
-            if (atlas.dim[0] != mri.dim[0] ||
-                atlas.dim[1] != mri.dim[1] ||
-                atlas.dim[2] != mri.dim[2]) {
-                return res.json({error:"the Atlas doesn't match with the mri"}).status(400).end();
+        /*
+        // check volume dimensions
+        if (atlas.dim[0] != mri.dim[0] ||
+            atlas.dim[1] != mri.dim[1] ||
+            atlas.dim[2] != mri.dim[2]) {
+            return res.json({error:"the Atlas doesn't match with the mri"}).status(400).end();
+        }
+        */
+
+        // create the atlas object
+        var date = new Date();
+        var atlasMetadata = {
+            name: atlasName,
+            project: atlasProject,
+            access: "edit", 
+            created: date.toJSON(), 
+            modified: date.toJSON(), 
+            filename: filename,	// automatically generated filename
+            originalname: files[0].originalname,
+            labels: atlasLabelSet,
+            owner: username,
+            type: "volume"
+        };
+        
+        console.log("final volume annotation entry:");
+        console.log(atlasMetadata);
+
+        // remove previous atlases with the same atlasName and atlasProject
+        var i;
+        for(i=mri.mri.atlas.length-1;i>=0;i--) {
+            if(mri.mri.atlas[i].name == atlasName && mri.mri.atlas[i].project == atlasProject) {
+                mri.mri.atlas.splice(i,1);
             }
+        }
 
-            // create the atlas object
-            var date = new Date();
-            var atlasMetadata = {
-                name: atlasName,
-                project: atlasProject,
-                access: "edit", 
-                created: date.toJSON(), 
-                modified: date.toJSON(), 
-                filename: filename,	// automatically generated filename
-                originalname: files[0].originalname,
-                labels: atlasLabelSet,
-                owner: username,
-                type: "volume"
-            };
-
-            // update the database
-            mri.mri.atlas.push(atlasMetadata);
-            // mark previous version as backup
-            req.db.get('mri').update({source:req.body.url,backup:{$exists:false}},{$set:{backup:true}},{multi:true})
-            .then(function() {
-                // insert new version
-                req.db.get('mri').insert(mri);
-            });
-
-            // return the full mri object ???
-            return res.json(mri).status(200).end();
-        })
-        .catch(function (err) {
-            return res.json({error:"mri file is not valid: "+err}).status(400).end();
+        // update the database
+        mri.mri.atlas.push(atlasMetadata);
+        // mark previous version as backup
+        req.db.get('mri').update({source:req.body.url,backup:{$exists:false}},{$set:{backup:true}},{multi:true})
+        .then(function() {
+            // insert new version
+            req.db.get('mri').insert(mri);
         });
+
+        // return the full mri object ???
+        return res.json(mri).status(200).end();
+    })
+    .catch(function (err) {
+        console.log("ERROR: mri file is not valid: ",err);
+        return res.json({error:"mri file is not valid: "+err}).status(400).end();
+    });
 }
 
 var token = function token(req, res) {
