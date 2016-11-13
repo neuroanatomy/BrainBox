d7bd2c321fd5a5e5254d71b9202f3112399f555e
roberto <roberto@golgi.local>
2016-11-13 17:08:01 +0100

    fix issue #106 : login sending you to wrong place when in home page

--- app.js
+++ app.js
@@ -134,6 +134,10 @@ app.get('/', function (req, res) { // /auth/github
     var login = (req.isAuthenticated()) ?
                 ("<a href='/user/" + req.user.username + "'>" + req.user.username + "</a> (<a href='/logout'>Log Out</a>)")
                 : ("<a href='/auth/github'>Log in with GitHub</a>");
+
+    // store return path in case of login
+    req.session.returnTo = req.originalUrl;
+
     res.render('index', {
         title: 'BrainBox',
         login: login
