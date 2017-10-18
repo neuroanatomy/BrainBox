c36f1acbc39c111d6a3fe486461c4508cc754291
r03ert0 <rto@braincatalogue.org>
2017-10-18 12:02:44 +0200

    replace tabs with 4 spaces

--- js/mri-loader.js
+++ js/mri-loader.js
@@ -6,171 +6,171 @@ const MRILoader = function () {
 
 /*
 Var loadNifti = function(nii) {
-	var	vox_offset=352;
-	var	sizeof_hdr=nii.readUInt32LE(0);
-	var	dimensions=nii.readUInt16LE(40);
+    var    vox_offset=352;
+    var    sizeof_hdr=nii.readUInt32LE(0);
+    var    dimensions=nii.readUInt16LE(40);
 
-	var mri={};
-	mri.hdr=nii.slice(0,vox_offset);
-	mri.dim=[];
-	mri.dim[0]=nii.readUInt16LE(42);
-	mri.dim[1]=nii.readUInt16LE(44);
-	mri.dim[2]=nii.readUInt16LE(46);
-	mri.datatype=nii.readUInt16LE(70);
-	mri.pixdim=[];
-	mri.pixdim[0]=nii.readFloatLE(80);
-	mri.pixdim[1]=nii.readFloatLE(84);
-	mri.pixdim[2]=nii.readFloatLE(88);
-	vox_offset=nii.readFloatLE(108);
+    var mri={};
+    mri.hdr=nii.slice(0,vox_offset);
+    mri.dim=[];
+    mri.dim[0]=nii.readUInt16LE(42);
+    mri.dim[1]=nii.readUInt16LE(44);
+    mri.dim[2]=nii.readUInt16LE(46);
+    mri.datatype=nii.readUInt16LE(70);
+    mri.pixdim=[];
+    mri.pixdim[0]=nii.readFloatLE(80);
+    mri.pixdim[1]=nii.readFloatLE(84);
+    mri.pixdim[2]=nii.readFloatLE(88);
+    vox_offset=nii.readFloatLE(108);
 
-	var tmp;
-	switch(mri.datatype) {
-		case 2: // UCHAR
-			mri.data=nii.slice(vox_offset);
-			break;
-		case 4: // SHORT
-			tmp=nii.slice(vox_offset);
-			mri.data=new Int16Array(mri.dim[0]*mri.dim[1]*mri.dim[2]);
-			for(var j=0;j<mri.dim[0]*mri.dim[1]*mri.dim[2];j++)
-				mri.data[j]=tmp.readInt16LE(j*2);
-			break;
-		case 8: // INT
-			tmp=nii.slice(vox_offset);
-			mri.data=new Uint32Array(mri.dim[0]*mri.dim[1]*mri.dim[2]);
-			for(var k=0;k<mri.dim[0]*mri.dim[1]*mri.dim[2];k++)
-				mri.data[k]=tmp.readUInt32LE(k*4);
-			break;
-		case 16: // FLOAT
-			tmp=nii.slice(vox_offset);
-			mri.data=new Float32Array(mri.dim[0]*mri.dim[1]*mri.dim[2]);
-			for(var l=0;l<mri.dim[0]*mri.dim[1]*mri.dim[2];l++)
-				mri.data[l]=tmp.readFloatLE(l*4);
-			break;
-		default:
-			console.log("ERROR: Unknown dataType: "+mri.datatype);
-	}
+    var tmp;
+    switch(mri.datatype) {
+        case 2: // UCHAR
+            mri.data=nii.slice(vox_offset);
+            break;
+        case 4: // SHORT
+            tmp=nii.slice(vox_offset);
+            mri.data=new Int16Array(mri.dim[0]*mri.dim[1]*mri.dim[2]);
+            for(var j=0;j<mri.dim[0]*mri.dim[1]*mri.dim[2];j++)
+                mri.data[j]=tmp.readInt16LE(j*2);
+            break;
+        case 8: // INT
+            tmp=nii.slice(vox_offset);
+            mri.data=new Uint32Array(mri.dim[0]*mri.dim[1]*mri.dim[2]);
+            for(var k=0;k<mri.dim[0]*mri.dim[1]*mri.dim[2];k++)
+                mri.data[k]=tmp.readUInt32LE(k*4);
+            break;
+        case 16: // FLOAT
+            tmp=nii.slice(vox_offset);
+            mri.data=new Float32Array(mri.dim[0]*mri.dim[1]*mri.dim[2]);
+            for(var l=0;l<mri.dim[0]*mri.dim[1]*mri.dim[2];l++)
+                mri.data[l]=tmp.readFloatLE(l*4);
+            break;
+        default:
+            console.log("ERROR: Unknown dataType: "+mri.datatype);
+    }
 
-	return mri;
+    return mri;
 }
 
 var loadBrainNifti = function(nii,callback) {
 
-	var brain=loadNifti(nii);
+    var brain=loadNifti(nii);
 
-	var i,sum=0,min,max;
-	min=brain.data[0];
-	max=min;
-	for(i=0;i<brain.dim[0]*brain.dim[1]*brain.dim[2];i++) {
-		sum+=brain.data[i];
+    var i,sum=0,min,max;
+    min=brain.data[0];
+    max=min;
+    for(i=0;i<brain.dim[0]*brain.dim[1]*brain.dim[2];i++) {
+        sum+=brain.data[i];
 
-		if(brain.data[i]<min) min=brain.data[i];
-		if(brain.data[i]>max) max=brain.data[i];
-	}
-	brain.sum=sum;
-	brain.min=min;
-	brain.max=max;
+        if(brain.data[i]<min) min=brain.data[i];
+        if(brain.data[i]>max) max=brain.data[i];
+    }
+    brain.sum=sum;
+    brain.min=min;
+    brain.max=max;
 
-	callback(brain);
+    callback(brain);
 }
 
 var loadBrainMGZ = function(data,callback) {
-	var hdr_sz=284;
-	var brain={};
-	var datatype;
+    var hdr_sz=284;
+    var brain={};
+    var datatype;
 
-	brain.dim=[];
-	brain.dim[0]=data.readInt32BE(4);
-	brain.dim[1]=data.readInt32BE(8);
-	brain.dim[2]=data.readInt32BE(12);
-	datatype=data.readInt32BE(20);
-	brain.pixdim=[];
-	brain.pixdim[0]=data.readFloatBE(30);
-	brain.pixdim[1]=data.readFloatBE(34);
-	brain.pixdim[2]=data.readFloatBE(38);
+    brain.dim=[];
+    brain.dim[0]=data.readInt32BE(4);
+    brain.dim[1]=data.readInt32BE(8);
+    brain.dim[2]=data.readInt32BE(12);
+    datatype=data.readInt32BE(20);
+    brain.pixdim=[];
+    brain.pixdim[0]=data.readFloatBE(30);
+    brain.pixdim[1]=data.readFloatBE(34);
+    brain.pixdim[2]=data.readFloatBE(38);
 
-	var tmp
-	switch(datatype) {
-		case 0: // MGHUCHAR
-			brain.data=data.slice(hdr_sz);
-			break;
-		case 1: // MGHINT
-			tmp=data.slice(hdr_sz);
-			brain.data=new Uint32Array(brain.dim[0]*brain.dim[1]*brain.dim[2]);
-			for(var j=0;j<brain.dim[0]*brain.dim[1]*brain.dim[2];j++)
-				brain.data[j]=tmp.readUInt32BE(j*4);
-			break;
-		case 3: // MGHFLOAT
-			tmp=data.slice(hdr_sz);
-			brain.data=new Float32Array(brain.dim[0]*brain.dim[1]*brain.dim[2]);
-			for(var k=0;k<brain.dim[0]*brain.dim[1]*brain.dim[2];k++)
-				brain.data[k]=tmp.readFloatBE(k*4);
-			break;
-		case 4: // MGHSHORT
-			tmp=data.slice(hdr_sz);
-			brain.data=new Int16Array(brain.dim[0]*brain.dim[1]*brain.dim[2]);
-			for(var l=0;l<brain.dim[0]*brain.dim[1]*brain.dim[2];l++)
-				brain.data[l]=tmp.readInt16BE(l*2);
-			break;
-		default:
-			console.log("ERROR: Unknown dataType: "+datatype);
-	}
-	var i,sum=0,min,max;
-	min=brain.data[0];
-	max=min;
-	for(i=0;i<brain.dim[0]*brain.dim[1]*brain.dim[2];i++) {
-		sum+=brain.data[i];
+    var tmp
+    switch(datatype) {
+        case 0: // MGHUCHAR
+            brain.data=data.slice(hdr_sz);
+            break;
+        case 1: // MGHINT
+            tmp=data.slice(hdr_sz);
+            brain.data=new Uint32Array(brain.dim[0]*brain.dim[1]*brain.dim[2]);
+            for(var j=0;j<brain.dim[0]*brain.dim[1]*brain.dim[2];j++)
+                brain.data[j]=tmp.readUInt32BE(j*4);
+            break;
+        case 3: // MGHFLOAT
+            tmp=data.slice(hdr_sz);
+            brain.data=new Float32Array(brain.dim[0]*brain.dim[1]*brain.dim[2]);
+            for(var k=0;k<brain.dim[0]*brain.dim[1]*brain.dim[2];k++)
+                brain.data[k]=tmp.readFloatBE(k*4);
+            break;
+        case 4: // MGHSHORT
+            tmp=data.slice(hdr_sz);
+            brain.data=new Int16Array(brain.dim[0]*brain.dim[1]*brain.dim[2]);
+            for(var l=0;l<brain.dim[0]*brain.dim[1]*brain.dim[2];l++)
+                brain.data[l]=tmp.readInt16BE(l*2);
+            break;
+        default:
+            console.log("ERROR: Unknown dataType: "+datatype);
+    }
+    var i,sum=0,min,max;
+    min=brain.data[0];
+    max=min;
+    for(i=0;i<brain.dim[0]*brain.dim[1]*brain.dim[2];i++) {
+        sum+=brain.data[i];
 
-		if(brain.data[i]<min) min=brain.data[i];
-		if(brain.data[i]>max) max=brain.data[i];
-	}
-	brain.sum=sum;
-	brain.min=min;
-	brain.max=max;
-	callback(brain);
+        if(brain.data[i]<min) min=brain.data[i];
+        if(brain.data[i]>max) max=brain.data[i];
+    }
+    brain.sum=sum;
+    brain.min=min;
+    brain.max=max;
+    callback(brain);
 }
 
 this.loadBrain = function(path,callback) {
-	if(!fs.existsSync(path)) {
-		console.log("ERROR: File does not exist:",path);					//modify call error object
-		return;
-	} else {
-		var datagz;
-		try {
-			datagz=fs.readFileSync(path);
-			var ft=fileType(datagz);
-			var ext=path.split('.').pop();
+    if(!fs.existsSync(path)) {
+        console.log("ERROR: File does not exist:",path);                    //modify call error object
+        return;
+    } else {
+        var datagz;
+        try {
+            datagz=fs.readFileSync(path);
+            var ft=fileType(datagz);
+            var ext=path.split('.').pop();
 
-			switch(ft.ext) {
-				case 'gz': {
-					switch(ext) {
-						case 'gz':
-								zlib.gunzip(datagz,function(err,nii){if(err) console.log("ERROR:",err);loadBrainNifti(nii,callback)});		//modify call error object
-							break;
-						case 'mgz':
-								zlib.gunzip(datagz,function(err,nii){if(err) console.log("ERROR:",err);loadBrainMGZ(nii,callback)});		//modify call error object
-							break;
-					}
-					break;
-				}
-				case 'zip':
-					zlib.inflate(datagz,function(err,nii){if(err) console.log("ERROR:",err);loadBrainNifti(nii,callback)});		//modify call error object
-					break;
-				default:
-					switch(ext) {
-						case 'nii':
-							loadBrainNifti(datagz,callback);
-							break;
-						case 'mgh':
-							loadBrainMGZ(datagz, callback);
-							break;
-					}
-					break;						//modify call error object
-			}
-		} catch(e) {
-			console.log(new Date(),"ERROR: Cannot read brain data");		//modify call error object
-		}
-	}
-	return null;
+            switch(ft.ext) {
+                case 'gz': {
+                    switch(ext) {
+                        case 'gz':
+                                zlib.gunzip(datagz,function(err,nii){if(err) console.log("ERROR:",err);loadBrainNifti(nii,callback)});        //modify call error object
+                            break;
+                        case 'mgz':
+                                zlib.gunzip(datagz,function(err,nii){if(err) console.log("ERROR:",err);loadBrainMGZ(nii,callback)});        //modify call error object
+                            break;
+                    }
+                    break;
+                }
+                case 'zip':
+                    zlib.inflate(datagz,function(err,nii){if(err) console.log("ERROR:",err);loadBrainNifti(nii,callback)});        //modify call error object
+                    break;
+                default:
+                    switch(ext) {
+                        case 'nii':
+                            loadBrainNifti(datagz,callback);
+                            break;
+                        case 'mgh':
+                            loadBrainMGZ(datagz, callback);
+                            break;
+                    }
+                    break;                        //modify call error object
+            }
+        } catch(e) {
+            console.log(new Date(),"ERROR: Cannot read brain data");        //modify call error object
+        }
+    }
+    return null;
 }
 */
 
--- public/js/atlasMaker-paint.js
+++ public/js/atlasMaker-paint.js
@@ -2,41 +2,41 @@
  * @page AtlasMaker: Painting commands
  */
 var AtlasMakerPaint = {
-	//====================================================================================
-	// Paint functions
-	//====================================================================================
-	showxy: function showxy(u,c,x,y,usr) {
-		var me=AtlasMakerWidget;
-		var l=me.traceLog(showxy,1,"#0c0");if(l)console.log.apply(undefined,l);
-	
-		// u: user number
-		// c: command
-		// x, y: coordinates
-		msg={c: c, x: x, y: y};
-		if(u==-1 && JSON.stringify(msg)!=JSON.stringify(me.msg0)) {
-			me.sendShowMessage(msg);
-			me.msg0=msg;
-		}
-		if(u!=-1) {
-		    switch(c) {
-		        case 'u':
-		            if(usr.pointer) {
+    //====================================================================================
+    // Paint functions
+    //====================================================================================
+    showxy: function showxy(u,c,x,y,usr) {
+        var me=AtlasMakerWidget;
+        var l=me.traceLog(showxy,1,"#0c0");if(l)console.log.apply(undefined,l);
+    
+        // u: user number
+        // c: command
+        // x, y: coordinates
+        msg={c: c, x: x, y: y};
+        if(u==-1 && JSON.stringify(msg)!=JSON.stringify(me.msg0)) {
+            me.sendShowMessage(msg);
+            me.msg0=msg;
+        }
+        if(u!=-1) {
+            switch(c) {
+                case 'u':
+                    if(usr.pointer) {
                         usr.pointer.remove();
                         delete usr.pointer;
                     }
-		            break;
-		        case 'm':
+                    break;
+                case 'm':
                     if(!usr.pointer) {
                         usr.pointer = $('<div style="display:inline-block;height:20px;margin-left:-10px;margin-top:-10px;position:absolute;color:white"><img src="/img/show.svg" height="100%"/>'+((usr.username == 'Anonymous')?u:usr.username)+'</div>');
                         $("#resizable").append(usr.pointer);
                     }
                     usr.pointer.css({left:x*$("#resizable").width()/me.brain_W,top:y*$("#resizable").height()/me.brain_H});
-		            break;
-		    }
-		}
-		usr.x0=x;
-		usr.y0=y;
-	},
+                    break;
+            }
+        }
+        usr.x0=x;
+        usr.y0=y;
+    },
     /**
      * @function paintxy
      * @desc Dispatches paint/erase commands to the annotation volume and to the server for broadcast
@@ -46,27 +46,27 @@ var AtlasMakerPaint = {
      * @param {integer} y Y coordinate in slice space
      * @param {Object} usr User object for the current user. Contains the painting value, view and slice
      */
-	paintxy: function paintxy(u,c,x,y,usr) {
-		var me=AtlasMakerWidget;
-		var l=me.traceLog(paintxy,1,"#0c0");if(l)console.log.apply(undefined,l);
-	
-		// u: user number
-		// c: command
-		// x, y: coordinates
-		msg={c:c,x:x,y:y};
-		
-		if(u==-1 && JSON.stringify(msg)!=JSON.stringify(me.msg0)) {
-			me.sendPaintMessage(msg);
-			me.msg0=msg;
-		}
-		
-		var	dim=me.atlas.dim;
-	
-		var	coord={x:x,y:y,z:usr.slice};
-		if(usr.x0<0) {
-			usr.x0=coord.x;
-			usr.y0=coord.y;
-		}
+    paintxy: function paintxy(u,c,x,y,usr) {
+        var me=AtlasMakerWidget;
+        var l=me.traceLog(paintxy,1,"#0c0");if(l)console.log.apply(undefined,l);
+    
+        // u: user number
+        // c: command
+        // x, y: coordinates
+        msg={c:c,x:x,y:y};
+        
+        if(u==-1 && JSON.stringify(msg)!=JSON.stringify(me.msg0)) {
+            me.sendPaintMessage(msg);
+            me.msg0=msg;
+        }
+        
+        var    dim=me.atlas.dim;
+    
+        var    coord={x:x,y:y,z:usr.slice};
+        if(usr.x0<0) {
+            usr.x0=coord.x;
+            usr.y0=coord.y;
+        }
 
         if(u!=-1) {
             if(c === 'mu') {
@@ -98,30 +98,30 @@ var AtlasMakerPaint = {
                 break;
         }
 
-		usr.x0=coord.x;
-		usr.y0=coord.y;
-	},
+        usr.x0=coord.x;
+        usr.y0=coord.y;
+    },
     /**
      * @function paintvol
      * @desc Paints a series of voxels as indicated in an array. This function is exclusively used for undoing
      * @param {Array} voxels Array where each object contains a voxel index and a voxel value. The voxel index goes from 0 to dim[0]*dim[1]*dim[2]-1
      */
-	paintvol: function paintvol(voxels) {
-		var me=AtlasMakerWidget;
-		var l=me.traceLog(paintvol,0,"#0c0");if(l)console.log.apply(undefined,l);
-	
-		var	i,
-			ind,			// voxel index
-			val;			// voxel delta-value, such that -=val undoes
-		for(i=0;i<voxels.length;i++) {
-			ind=voxels[i][0];
-			val=voxels[i][1];
-			
-			me.atlas.data[ind]=val;
-		}
+    paintvol: function paintvol(voxels) {
+        var me=AtlasMakerWidget;
+        var l=me.traceLog(paintvol,0,"#0c0");if(l)console.log.apply(undefined,l);
+    
+        var    i,
+            ind,            // voxel index
+            val;            // voxel delta-value, such that -=val undoes
+        for(i=0;i<voxels.length;i++) {
+            ind=voxels[i][0];
+            val=voxels[i][1];
+            
+            me.atlas.data[ind]=val;
+        }
 
-		me.drawImages();
-	},
+        me.drawImages();
+    },
     /**
      * @function fill
      * @desc Fills a 2D slice in an annotation volume starting at coordinates x, y, z replacing all the connected pixels of same value as the original value at x, y, z
@@ -131,152 +131,152 @@ var AtlasMakerPaint = {
      * @param {Integer} val Value to fill with
      * @param {String} myView The stereotaxic plane along which to fill: either 'cor', 'axi' or 'sag'
      */
-	fill: function fill(x,y,z,val,myView) {
-		var me=AtlasMakerWidget;
-		var l=me.traceLog(fill,0,"#0c0");if(l)console.log.apply(undefined,l);
-	
-		var	atlas=me.atlas;
-		var	dim=atlas.dim;
+    fill: function fill(x,y,z,val,myView) {
+        var me=AtlasMakerWidget;
+        var l=me.traceLog(fill,0,"#0c0");if(l)console.log.apply(undefined,l);
+    
+        var    atlas=me.atlas;
+        var    dim=atlas.dim;
 
-		var	Q=[],
-		    left,
-		    right,
-		    n;
-		var	i,max=0;
-		var bval=atlas.data[me.slice2index(x,y,z,myView)]; // background-value: value of the voxel where the click occurred
-		
-		if(bval==val)	// nothing to do
-			return;
-		
-		Q.push({x:x,y:y});
-		while(Q.length>0) {
-		    if(Q.length>max)
-		        max=Q.length;
-			n=Q.shift();
-			if(atlas.data[me.slice2index(n.x,n.y,z,myView)]!=bval)
-			    continue;
-			left=n.x;
-			right=n.x;
-			y=n.y;
-			while (left-1>=0 && atlas.data[me.slice2index(left-1,y,z,myView)]==bval) {
+        var    Q=[],
+            left,
+            right,
+            n;
+        var    i,max=0;
+        var bval=atlas.data[me.slice2index(x,y,z,myView)]; // background-value: value of the voxel where the click occurred
+        
+        if(bval==val)    // nothing to do
+            return;
+        
+        Q.push({x:x,y:y});
+        while(Q.length>0) {
+            if(Q.length>max)
+                max=Q.length;
+            n=Q.shift();
+            if(atlas.data[me.slice2index(n.x,n.y,z,myView)]!=bval)
+                continue;
+            left=n.x;
+            right=n.x;
+            y=n.y;
+            while (left-1>=0 && atlas.data[me.slice2index(left-1,y,z,myView)]==bval) {
                 left--;
             }
-			while (right+1<me.brain_W && atlas.data[me.slice2index(right+1,y,z,myView)]==bval) {
+            while (right+1<me.brain_W && atlas.data[me.slice2index(right+1,y,z,myView)]==bval) {
                 right++;
             }
             for(x=left;x<=right;x++) {
                 atlas.data[me.slice2index(x,y,z,myView)]=val;
-				if(y-1>=0         && atlas.data[me.slice2index(x,y-1,z,myView)]==bval)
-					Q.push({x:x,y:y-1});
-				if(y+1<me.brain_H && atlas.data[me.slice2index(x,y+1,z,myView)]==bval)
-					Q.push({x:x,y:y+1});
+                if(y-1>=0         && atlas.data[me.slice2index(x,y-1,z,myView)]==bval)
+                    Q.push({x:x,y:y-1});
+                if(y+1<me.brain_H && atlas.data[me.slice2index(x,y+1,z,myView)]==bval)
+                    Q.push({x:x,y:y+1});
             }
-		}
-		me.drawImages();
-		console.log("max array size for fill:",max);
-	},
+        }
+        me.drawImages();
+        console.log("max array size for fill:",max);
+    },
     /**
      * @function line
      */
-	line: function line(x,y,val,usr) {
-		var me=AtlasMakerWidget;
-		var l=me.traceLog(line,1,"#0c0");if(l)console.log.apply(undefined,l);
-	
-		// Bresenham's line algorithm adapted from
-		// http://stackoverflow.com/questions/4672279/bresenham-algorithm-in-javascript
+    line: function line(x,y,val,usr) {
+        var me=AtlasMakerWidget;
+        var l=me.traceLog(line,1,"#0c0");if(l)console.log.apply(undefined,l);
+    
+        // Bresenham's line algorithm adapted from
+        // http://stackoverflow.com/questions/4672279/bresenham-algorithm-in-javascript
 
-		var	atlas=me.atlas;
-		var	dim=atlas.dim;
-		var	xyzi1=new Array(4);
-		var	xyzi2=new Array(4);
-		var	i;
-		var	x1=usr.x0;
-		var y1=usr.y0;
-		var x2=x;
-		var y2=y;
-		var	z=usr.slice;
+        var    atlas=me.atlas;
+        var    dim=atlas.dim;
+        var    xyzi1=new Array(4);
+        var    xyzi2=new Array(4);
+        var    i;
+        var    x1=usr.x0;
+        var y1=usr.y0;
+        var x2=x;
+        var y2=y;
+        var    z=usr.slice;
 
-		// Define differences and error check
-		var dx = Math.abs(x2 - x1);
-		var dy = Math.abs(y2 - y1);
-		var sx = (x1 < x2) ? 1 : -1;
-		var sy = (y1 < y2) ? 1 : -1;
-		var err = dx - dy;
+        // Define differences and error check
+        var dx = Math.abs(x2 - x1);
+        var dy = Math.abs(y2 - y1);
+        var sx = (x1 < x2) ? 1 : -1;
+        var sy = (y1 < y2) ? 1 : -1;
+        var err = dx - dy;
 
-		xyzi1=me.slice2xyzi(x1,y1,z,usr.view);
-		xyzi2=me.slice2xyzi(x2,y2,z,usr.view);
-		me.annotationLength+=Math.sqrt(	Math.pow(me.brain_pixdim[0]*(xyzi1[0]-xyzi2[0]),2)+
-										Math.pow(me.brain_pixdim[1]*(xyzi1[1]-xyzi2[1]),2)+
-										Math.pow(me.brain_pixdim[2]*(xyzi1[2]-xyzi2[2]),2));
-	
-		for(j=0;j<Math.min(usr.penSize,me.brain_W-x1);j++)
-		for(k=0;k<Math.min(usr.penSize,me.brain_H-y1);k++) {
-			i=me.slice2index(x1+j,y1+k,z,usr.view);
-			atlas.data[i]=val;
-		}
-	
-		while (!((x1 == x2) && (y1 == y2))) {
-			var e2 = err << 1;
-			if (e2 > -dy) {
-				err -= dy;
-				x1 += sx;
-			}
-			if (e2 < dx) {
-				err += dx;
-				y1 += sy;
-			}
+        xyzi1=me.slice2xyzi(x1,y1,z,usr.view);
+        xyzi2=me.slice2xyzi(x2,y2,z,usr.view);
+        me.annotationLength+=Math.sqrt(    Math.pow(me.brain_pixdim[0]*(xyzi1[0]-xyzi2[0]),2)+
+                                        Math.pow(me.brain_pixdim[1]*(xyzi1[1]-xyzi2[1]),2)+
+                                        Math.pow(me.brain_pixdim[2]*(xyzi1[2]-xyzi2[2]),2));
+    
+        for(j=0;j<Math.min(usr.penSize,me.brain_W-x1);j++)
+        for(k=0;k<Math.min(usr.penSize,me.brain_H-y1);k++) {
+            i=me.slice2index(x1+j,y1+k,z,usr.view);
+            atlas.data[i]=val;
+        }
+    
+        while (!((x1 == x2) && (y1 == y2))) {
+            var e2 = err << 1;
+            if (e2 > -dy) {
+                err -= dy;
+                x1 += sx;
+            }
+            if (e2 < dx) {
+                err += dx;
+                y1 += sy;
+            }
             for(j=0;j<Math.min(usr.penSize,me.brain_W-x1);j++)
             for(k=0;k<Math.min(usr.penSize,me.brain_H-y1);k++) {
-				i=me.slice2index(x1+j,y1+k,z,usr.view);
-				atlas.data[i]=val;
-			}
-		}
-		me.drawImages();
-	},
+                i=me.slice2index(x1+j,y1+k,z,usr.view);
+                atlas.data[i]=val;
+            }
+        }
+        me.drawImages();
+    },
     /**
      * @function slice2index
      */
-	slice2index: function slice2index(mx,my,mz,myView) {
-		var me=AtlasMakerWidget;
-		var l=me.traceLog(slice2index,3,"#0c0");if(l)console.log.apply(undefined,l);
-	
-		var	dim=me.atlas.dim;
-		var	x,y,z,i;
-		switch(myView) {
-			case 'sag':	x=mz; y=mx; z=me.brain_H-1-my;break; // sagital
-			case 'cor':	x=mx; y=mz; z=me.brain_H-1-my;break; // coronal
-			case 'axi':	x=mx; y=me.brain_H-1-my; z=mz;break; // axial
-		}
+    slice2index: function slice2index(mx,my,mz,myView) {
+        var me=AtlasMakerWidget;
+        var l=me.traceLog(slice2index,3,"#0c0");if(l)console.log.apply(undefined,l);
+    
+        var    dim=me.atlas.dim;
+        var    x,y,z,i;
+        switch(myView) {
+            case 'sag':    x=mz; y=mx; z=me.brain_H-1-my;break; // sagital
+            case 'cor':    x=mx; y=mz; z=me.brain_H-1-my;break; // coronal
+            case 'axi':    x=mx; y=me.brain_H-1-my; z=mz;break; // axial
+        }
 /*
-	TRANSFORM SCREEN SPACE INTO VOXEL INDEX
+    TRANSFORM SCREEN SPACE INTO VOXEL INDEX
 */
-		var s=[x,y,z];
-		i=me.S2I(s,me.User);
+        var s=[x,y,z];
+        i=me.S2I(s,me.User);
 
-		return i;
-	},
+        return i;
+    },
     /**
      * @function slice2xyzi
      * @desc Convert slice coordinates into voxel coordinates
      * @return An array [x,y,z,i] where the first 3 values are the voxel coordinates and the 4th value is the voxel index (value from 0 to dim[0]*dim[1]*dim[2]-1)
      */
-	slice2xyzi: function slice2xyzi(mx,my,mz,myView) {
-		var me=AtlasMakerWidget;
-		var l=me.traceLog(slice2xyzi,1,"#0c0");if(l)console.log.apply(undefined,l);
-	
-		var	dim=me.atlas.dim;
-		var	x,y,z,i;
-		switch(myView) {
-			case 'sag':	x=mz; y=mx; z=me.brain_H-1-my;break; // sagital
-			case 'cor':	x=mx; y=mz; z=me.brain_H-1-my;break; // coronal
-			case 'axi':	x=mx; y=me.brain_H-1-my; z=mz;break; // axial
-		}
+    slice2xyzi: function slice2xyzi(mx,my,mz,myView) {
+        var me=AtlasMakerWidget;
+        var l=me.traceLog(slice2xyzi,1,"#0c0");if(l)console.log.apply(undefined,l);
+    
+        var    dim=me.atlas.dim;
+        var    x,y,z,i;
+        switch(myView) {
+            case 'sag':    x=mz; y=mx; z=me.brain_H-1-my;break; // sagital
+            case 'cor':    x=mx; y=mz; z=me.brain_H-1-my;break; // coronal
+            case 'axi':    x=mx; y=me.brain_H-1-my; z=mz;break; // axial
+        }
 /*
-	TRANSFORM SCREEN SPACE INTO VOXEL INDEX
+    TRANSFORM SCREEN SPACE INTO VOXEL INDEX
 */
-		var s=[x,y,z];
-		i=me.S2I(s,me.User);
+        var s=[x,y,z];
+        i=me.S2I(s,me.User);
 
-		return [x,y,z,i];	
-	}
+        return [x,y,z,i];    
+    }
 };
