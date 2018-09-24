ff2a9bd8d39f6419fcb31bff16a17cd90bbbd3da
Roberto Toro <roberto.toro@gmail.com>
2018-09-24 11:11:37 +0200

    changing stuff and seeing what happens

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 # Exit script if you try to use an uninitialized variable.
-set -o nounset
+#set -o nounset
 
 # Exit script if a statement returns a non-true return value.
-set -o errexit
+#set -o errexit
 
 # Use the error status of the first failure, rather than that of the last item in a pipeline.
-set -o pipefail
+#set -o pipefail
 
 cd scripts || return
 
