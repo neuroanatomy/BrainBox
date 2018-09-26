294c1983ad0ef260dc3fb4a81f4c40a9cc1a1c02
Roberto Toro <roberto.toro@gmail.com>
2018-09-26 07:18:49 +0200

    start migration to webpack

---
+++ view/brainbox/src/pages/project-new-page.js
@@ -0,0 +1,42 @@
+var host = "ws://" + window.location.hostname + ":8080/";
+if (window.WebSocket) {
+    ws=new WebSocket(host);
+} else if (window.MozWebSocket) {
+    ws=new MozWebSocket(host);
+}
+ws.onopen = function(msg) {
+    ws.send(JSON.stringify({"type":"autocompleteClient"}));
+}
+ws.onmessage = function(message) {
+    message = JSON.parse(message.data);
+    if (message.type === "projectNameQuery") {
+        if(message.metadata) {
+            $("#warning").html("The project <a><strong>"+message.metadata.shortname+"</strong></a> already exists");
+            $("#warning a").attr('href','/project/'+message.metadata.shortname);
+            $("#warning").show();
+            $("#createProject").css({'pointer-events':'none',opacity:0.5});
+        } else {
+            $("#warning").hide();
+            $("#createProject").css({'pointer-events':'auto',opacity:1});
+        }
+    }
+}
+
+$("#projectName").on('keyup',function(e) {
+    var name=DOMPurify.sanitize($("#projectName").val());
+    
+    // check if name is alphanumeric
+    if(/[^a-zA-Z0-9]+/.test(name) === true) {
+        $("#warning").html("The name <strong>"+name+"</strong> is not allowed. Project short names can only contain letters and numbers");
+        $("#warning").show();
+        $("#createProject").css({'pointer-events':'none',opacity:0.5});
+    } else {
+    // check if name already exists
+        ws.send(JSON.stringify({"type":"projectNameQuery", "metadata":{"name":name}}));
+    }
+});
+
+$("#createProject").click(function cancelChanges(){location.pathname='/project/'+$("#projectName").val()+'/settings'});
+$("#cancelChanges").click(function cancelChanges(){location.pathname='{{{projectURL}}}'});
+
+$("#addProject").click(function(){location="/project/new"});
