7f5080f9a55b982f647e3988fd2d94555cb9aa6f
r03ert0 <rto@braincatalogue.org>
2020-04-19 12:10:54 +0200

    use chai assert, avoid arrow functions, better wait for data

--- test/integration/mri.controller.test.js
+++ test/integration/mri.controller.test.js
@@ -1,39 +1,66 @@
 'use strict';
 
 const fs = require('fs');
-var assert = require('chai').assert;
 const chai = require('chai');
+var assert = chai.assert;
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 const U = require('../utils.js');
 
-describe('TESTING THE /mri ROUTE', async () => {
-  after(async () => {
-    // remove MRI from db and file system
-    const res = await chai.request(U.serverURL).post('/mri/json').send({
-      url: U.localBertURL,
-      token: U.testToken
-    });
-    const {body} = res;
-    const dirPath = "./public" + body.url;
-    await U.removeMRI({dirPath, srcURL: U.localBertURL});
-  });
-
-  describe('/mri route', async () => {
-    it('get("/") should return status 200', async () => {
+describe('TESTING THE /mri ROUTE', function () {
+  describe('/mri route', function () {
+    it('get("/") should return status 200', async function () {
       const res = await chai.request(U.serverURL)
         .get('/');
       assert.equal(res.statusCode, 200);
     });
 
-    it('Should return an error message requesting page for an empty GET /mri/json/', async () => {
+    it('Should return an error message requesting page for an empty GET /mri/json/', async function () {
       const res = await chai.request(U.serverURL).get('/mri/json/');
       const {body} = res;
       const expected = {error: "Provide the parameter 'page'"};
       assert.deepEqual(body, expected);
     });
 
-    it('GET /mri/json?page=0 should return an array with >=1 file', async () => {
+    it('POST /mri/json without parameters should fail', async function () {
+      await chai.request(U.serverURL).get('/0--------------------------------');
+      const res = await chai.request(U.serverURL).post('/mri/json');
+      assert.equal(res.statusCode, 403);
+    });
+
+    it('POST /mri/json with url should start a download', async function () {
+      await chai.request(U.serverURL).get('/A--------------------------------');
+      let shouldContinue = true;
+      let body, res;
+      while(shouldContinue) {
+        res = await chai.request(U.serverURL).post('/mri/json').send({
+          url: U.localBertURL,
+          token: U.testToken + U.userFoo.nickname
+        });
+        body = res.body;
+        // console.log(body);
+        shouldContinue = (body.success !== true);
+
+        await U.delay(U.shortTimeout);
+      }
+      // console.log(body.success, shouldContinue, body);
+      assert.equal(res.statusCode, 200);
+    }).timeout(U.longTimeout);
+
+    it('POST /mri/json with url should return MRI info once the file is downloaded', async function () {
+      await chai.request(U.serverURL).get('/B--------------------------------');
+      await U.delay(U.shortTimeout);
+      const res = await chai.request(U.serverURL).post('/mri/json').send({
+        url: U.localBertURL,
+        token: U.testToken + U.userFoo.nickname
+      });
+      const {body} = res;
+      // console.log(body);
+      assert.equal(body.success, true);
+      assert.equal(res.statusCode, 200);
+    }).timeout(U.mediumTimeout);
+
+    it('GET /mri/json?page=0 should return an array with >=1 file', async function () {
       const res = await chai.request(U.serverURL).get('/mri/json?page=0')
           .query({page: 0});
       const {body} = res;
@@ -41,64 +68,27 @@ describe('TESTING THE /mri ROUTE', async () => {
       assert.isAtLeast(body.length, 1);
     });
 
-    it('POST /mri/json without parameters should fail', async () => {
-      const res = await chai.request(U.serverURL).post('/mri/json');
-      assert.equal(res.statusCode, 403);
-    });
-
-    // It's fine to post /mri/json without being authenticated
-    // it('POST /mri/json with token but without url should fail', async () => {
-    //   const res = await chai.request(U.serverURL).post('/mri/json').send({token: U.testToken});
-    //   assert.equal(res.statusCode, 403);
-    // });
-
-    // it('POST /mri/json with url but without token should fail', async () => {
-    //   const res = await chai.request(U.serverURL).post('/mri/json').send({url: U.localBertURL});
-    //   assert.equal(res.statusCode, 403);
-    // });
-
-    it('POST /mri/json with url should start a download', async () => {
-      let res = await chai.request(U.serverURL).post('/mri/json').send({
-        url: U.localBertURL,
-        token: U.testToken
-      });
-      let {body} = res;
-      if(body.success === true) {
-        // console.log("MRI was already downloaded. Rimraf it and start again");
-        const dirPath = "./public" + body.url;
-        await U.removeMRI({dirPath, srcURL: U.localBertURL});
-      }
-
-      res = await chai.request(U.serverURL).post('/mri/json').send({
-        url: U.localBertURL,
-        token: U.testToken
-      });
-      body = res.body;
-      assert.equal(body.success, "downloading");
-      assert.equal(res.statusCode, 200);
-    });
-
-    it('POST /mri/json with url should return MRI info once the file is downloaded', async () => {
-      await U.delay(U.shortTimeout);
-      const res = await chai.request(U.serverURL).post('/mri/json').send({
-        url: U.localBertURL
-      });
-      const {body} = res;
-      assert.equal(body.success, true);
-      assert.equal(res.statusCode, 200);
-    }).timeout(U.mediumTimeout);
-
-    it('GET /mri/json should provide MRI info for an existing file', async () => {
+    it('GET /mri/json should provide MRI info for an existing file', async function () {
       const res = await chai.request(U.serverURL).get('/mri/json')
           .query({url: U.localBertURL});
       const {body} = res;
       const expectedKeys = [
         "_id", "filename", "success", "source", "url", "included",
         "dim", "pixdim", "voxel2world", "worldOrigin",
-        "owner", "mri" // optional keys: "modified", "modifiedBy", "name"
+        "owner", "mri", "modified", "modifiedBy", "name"
       ];
       assert.isObject(body);
       assert.hasAllKeys(body, expectedKeys);
+    });
+
+    it('Remove test MRI from db and disk', async function () {
+      // remove the MRI
+      const res = await chai.request(U.serverURL).get('/mri/json').query({
+        url: U.localBertURL
+      });
+      const {body} = res;
+      const dirPath = "./public" + body.url;
+      await U.removeMRI({dirPath, srcURL: U.localBertURL});
     });
   });
 });
