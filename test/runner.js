ccecfcf2f0c17c2507d63fc7dad822a7475fafa3
r03ert0 <rto@braincatalogue.org>
2020-04-19 12:07:29 +0200

    avoid arrow functions in mocha

--- test/runner.js
+++ test/runner.js
@@ -1,14 +1,14 @@
 const browser = require('./browser');
 const U = require('./utils.js');
 
-before(async () => {
+before(async function () {
   await U.insertUser(U.userFoo);
   await U.insertProject(U.projectTest);
   await U.insertTestTokenForUser("foo");
   await browser.init();
 });
 
-after(async () => {
+after(async function () {
   await U.removeUser(U.userFoo.nickname);
   await U.removeProject(U.projectTest.shortname);
   await U.removeTestTokenForUser("foo");
