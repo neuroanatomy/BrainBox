d8efb12765a1d987c4a5b750370338ffa09ea3f0
r03ert0 <rto@braincatalogue.org>
2020-04-21 17:08:59 +0200

    add new commands

---
+++ view/atlasmaker/src/tools/commands.js
@@ -0,0 +1,10 @@
+window.commands = (cmd) => {
+    if(cmd === "help") { return "List all available commands"; }
+
+    fetch("/lib/atlasmaker-tools/tools.json")
+        .then(r=>r.text())
+        .then(t=>JSON.parse(t))
+        .then(f=>f.filter(o=>o.type === "cmd"))
+        .then(f => f.map(o=>o.name).join("\n"))
+        .then(t => console.log(t));
+};
---
+++ view/atlasmaker/src/tools/put_label.js
@@ -0,0 +1,14 @@
+window.put_label = (cmd) => {
+    if(cmd === "help") { return "({vox: array}) Puts the label in the current volume"; }
+
+    const me = AtlasMakerWidget;
+    const {data} = me.atlas;
+    const {vox} = cmd;
+
+    console.time('put_label');
+    me.paintvol(vox);
+    console.timeEnd('put_label');
+
+    me.sendAtlasDataMessage(data);
+    me.drawImages();
+};
