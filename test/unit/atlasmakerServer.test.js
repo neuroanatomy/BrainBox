6b51fea5f8b7b7ac85f35055cc8dd0072a463189
Nicolas <ntraut9@gmail.com>
2021-07-07 14:28:43 +0200

    various fixes

--- test/unit/atlasmakerServer.test.js
+++ test/unit/atlasmakerServer.test.js
@@ -11,19 +11,13 @@ const U = require('../utils.js');
 const { expect } = require('chai');
 require('mocha-sinon');
 
-// console.log("Current directory:", __dirname);
-// const { exec } = require("child_process");
-// exec('ls -l', (error, stdout, std_err) => {
-//   console.log(stdout);
-// });
-
 describe('UNIT TESTING ATLASMAKER SERVER', function () {
   describe('MRI IO', function () {
     let mri1, mri2;
 
     it('Should load a nii.gz file', async function () {
       mri1 = await amri.readNifti(datadir + 'bert_brain.nii.gz');
-    });
+    }).timeout(U.mediumTimeout);
 
     it('Should get the dimensions right', function () {
       assert(mri1.dim[0] === 256 && mri1.dim[1] === 256 && mri1.dim[2] === 256);
@@ -61,7 +55,7 @@ describe('UNIT TESTING ATLASMAKER SERVER', function () {
   // Function to test loadMRI function on different inputs
   describe('loadMRI function ', function () {
     it('should load the contents of .nii.gz file when a valid path is passed', async function (done) {
-      const path = __dirname.split('/unit')[0] + '/data/bert_brain.nii.gz';
+      const path = datadir + 'bert_brain.nii.gz';
       amri.loadMRI(path).then((res) => {
         expect(res).to.not.eql(null);
         expect(res).to.haveOwnProperty('dim');
@@ -84,7 +78,7 @@ describe('UNIT TESTING ATLASMAKER SERVER', function () {
     });
 
     it('should load the contents of .mgz file when a valid path is passed', async function (done) {
-      const path = __dirname.split('/unit')[0] + '/data/001.mgz';
+      const path = datadir + '001.mgz';
       amri.loadMRI(path).then((res) => {
         expect(res).to.not.eql(null);
         expect(res).to.haveOwnProperty('dim');
@@ -184,13 +178,13 @@ describe('UNIT TESTING ATLASMAKER SERVER', function () {
       it('should return correct value if the mri path is valid and not being used', async function () {
         let ws = new WebSocket({ port: 8081 });
         await AMS._connectNewUser({ ws: ws });
-        const path = __dirname.split('/unit')[0] + '/data/001.mgz';
+        const path = datadir + '001.mgz';
         await amri.loadMRI(path);
         const users = await AMS.numberOfUsersConnectedToMRI(path);
         await AMS._disconnectUser({ ws: ws });
         ws.close();
         assert.strictEqual(users, 0);
-      });
+      }).timeout(U.longTimeout);
     });
 
     describe('displayUsers function() ', function () {
@@ -233,7 +227,7 @@ describe('UNIT TESTING ATLASMAKER SERVER', function () {
       });
 
       it('should display the brains when there are some brains loaded', async function () {
-        let path = __dirname.split('/unit')[0] + '/data/001.mgz';
+        let path = datadir + '001.mgz';
         let brain = await AMS.getBrainAtPath(path);
         await AMS.displayBrains();
         let cnt = 0;
@@ -243,7 +237,7 @@ describe('UNIT TESTING ATLASMAKER SERVER', function () {
         }
         await AMS.unloadMRI(path);
         assert.strictEqual(cnt, 1);
-      });
+      }).timeout(U.mediumTimeout);;
     });
 
     describe('getBrainAtPath function() ', function () {
@@ -254,7 +248,7 @@ describe('UNIT TESTING ATLASMAKER SERVER', function () {
       });
 
       it('should load the brain if the path is valid', function (done) {
-        const path = __dirname.split('/unit')[0] + '/data/001.mgz';
+        const path = datadir + '001.mgz';
         AMS.getBrainAtPath(path).then((res) => {
           console.log(res);
           expect(res).to.not.eql(null);
@@ -278,27 +272,27 @@ describe('UNIT TESTING ATLASMAKER SERVER', function () {
     });
 
     describe('_connectNewUser() function ', function () {
-      it('should connect the user when the web socket is passed', async function() {
+      it('should connect the user when the web socket is passed', async function () {
         let ws = new WebSocket({ port: 8081 });
         await AMS._connectNewUser({ ws: ws });
         let cnt = 0;
-        for(var x = 0; x < AMS.US.length; x++) {
-          if(AMS.US[x])
+        for (var x = 0; x < AMS.US.length; x++) {
+          if (AMS.US[x])
             cnt++;
         }
-        await AMS._disconnectUser({ws: ws});
+        await AMS._disconnectUser({ ws: ws });
         ws.close();
         assert.strictEqual(cnt, 1);
       });
     });
     describe('removeUser() function ', function () {
-      it('should remove the user with the provided websocket', async function() {
+      it('should remove the user with the provided websocket', async function () {
         let ws = new WebSocket({ port: 8081 });
         await AMS._connectNewUser({ ws: ws });
         await AMS.removeUser(ws);
         let cnt = 0;
-        for(var x = 0; x < AMS.US.length; x++) {
-          if(AMS.US[x])
+        for (var x = 0; x < AMS.US.length; x++) {
+          if (AMS.US[x])
             cnt++;
         }
         ws.close();
