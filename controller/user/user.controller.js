f3c587b196e2407f45489df378f2528879c7be6b
Aditya R Rudra <adityaberasia@gmail.com>
2021-07-13 16:47:51 +0530

    Unit tests for User Controller added..

--- controller/user/user.controller.js
+++ controller/user/user.controller.js
@@ -3,7 +3,7 @@
 const dateFormat = require('dateformat');
 const dataSlices = require('../dataSlices/dataSlices.js');
 
-const validator = function(req, res, next) {
+const validator = function (req, res, next) {
 
   // userName can be an ip address (for anonymous users)
 
@@ -20,7 +20,7 @@ const validator = function(req, res, next) {
   next();
 };
 
-const user = function(req, res) {
+const user = async function (req, res) {
   var login = (req.isAuthenticated()) ?
     ("<a href='/user/" + req.user.username + "'>" + req.user.username + "</a> (<a href='/logout'>Log Out</a>)")
     : ("<a href='/auth/github'>Log in with GitHub</a>");
@@ -29,52 +29,48 @@ const user = function(req, res) {
   // store return path in case of login
   req.session.returnTo = req.originalUrl;
 
-  req.db.get('user').findOne({nickname: requestedUser}, "-_id")
-    .then(function (json) {
-      if(json) {
-        var context = {
-          username: json.name,
-          nickname: json.nickname,
-          joined: dateFormat(json.joined, "dddd d mmm yyyy, HH:MM"),
-          avatar: json.avatarURL,
-          title: requestedUser,
-          userInfo: JSON.stringify(json),
-          tab: req.query.tab||"mri",
-          login: login
-        };
-        res.render('user', context);
-      } else {
-        res.status(404).send("User Not Found");
-      }
-    })
-    .catch(function(err) {
+  const json = await req.db.get('user').findOne({ nickname: requestedUser })
+    .catch(function (err) {
       console.log("ERROR:", err);
       res.status(400).send("Error");
     });
+  if (json) {
+    var context = {
+      username: json.name,
+      nickname: json.nickname,
+      joined: dateFormat(json.joined, "dddd d mmm yyyy, HH:MM"),
+      avatar: json.avatarURL,
+      title: requestedUser,
+      userInfo: JSON.stringify(json),
+      tab: req.query.tab || "mri",
+      login: login
+    };
+    res.render('user', context);
+  } else {
+    res.status(404).send("User Not Found");
+  }
 };
 
-const apiUser = function(req, res) {
-  req.db.get('user').findOne({nickname: req.params.userName, backup: {$exists: false}}, "-_id")
-    .then(function (json) {
-      if (json) {
-        if (req.query.var) {
-          const arr = req.query.var.split("/");
-          for (const i in arr) {
-            if({}.hasOwnProperty.call(arr, i)) {
-              json = json[arr[i]];
-            }
-          }
+const apiUser = async function (req, res) {
+  let json = await req.db.get('user').findOne({ nickname: req.params.userName, backup: { $exists: false } }, "-_id");
+  if (json) {
+    if (req.query.var) {
+      const arr = req.query.var.split("/");
+      for (const i in arr) {
+        if ({}.hasOwnProperty.call(arr, i)) {
+          json = json[arr[i]];
         }
-        res.send(json);
-      } else {
-        res.send();
       }
-    });
+    }
+    res.send(json);
+  } else {
+    res.send();
+  }
 };
 
-const apiUserAll = function(req, res) {
-  if(!req.query.page) {
-    res.json({error:"Provide the parameter 'page'"});
+const apiUserAll = async function (req, res) {
+  if (!req.query.page) {
+    res.json({ error: "Provide the parameter 'page'" });
 
     return;
   }
@@ -83,12 +79,10 @@ const apiUserAll = function(req, res) {
   var page = parseInt(req.query.page);
   var nItemsPerPage = 20;
 
-  req.db.get('user').find({backup: {$exists: false}}, {skip: page*nItemsPerPage, limit: nItemsPerPage, fields:{_id:0}})
-    .then(function (json) {
-      res.send(json.map(function(o) {
-        return o.nickname;
-      }));
-    });
+  const json = await req.db.get('user').find({ backup: { $exists: false } }, { skip: page * nItemsPerPage, limit: nItemsPerPage, fields: { _id: 0 } });
+  res.send(json.map(function (o) {
+    return o.nickname;
+  }));
 };
 
 
@@ -98,20 +92,20 @@ const apiUserAll = function(req, res) {
  * @param {Object} res Response object
  * @returns {Object} Object with a list of user mri files
  */
-const apiUserFiles = function(req, res) {
+const apiUserFiles = async function (req, res) {
   // @todo Check access rights for this route
-  const {userName} = req.params;
-  let {start, length} = req.query;
+  const { userName } = req.params;
+  let { start, length } = req.query;
 
   console.log("userName:", userName, "start:", start, "length:", length);
 
-  if(typeof start === "undefined") {
-    res.status(403).send({error: "Provide 'start'"});
+  if (typeof start === "undefined") {
+    res.status(403).send({ error: "Provide 'start'" });
 
     return;
   }
-  if(typeof length === "undefined") {
-    res.status(403).send({error: "Provide 'length'"});
+  if (typeof length === "undefined") {
+    res.status(403).send({ error: "Provide 'length'" });
 
     return;
   }
@@ -119,14 +113,12 @@ const apiUserFiles = function(req, res) {
   start = parseInt(start);
   length = parseInt(length);
 
-  dataSlices.getUserFilesSlice(req, userName, start, length)
-    .then(function(result) {
-      res.send(result);
-    })
-    .catch(function(err) {
+  const result = await dataSlices.getUserFilesSlice(req, userName, start, length)
+    .catch(function (err) {
       console.log("ERROR:", err);
-      res.send({success:false, list:[]});
+      res.send({ success: false, list: [] });
     });
+  res.send(result);
 };
 
 /**
@@ -135,33 +127,31 @@ const apiUserFiles = function(req, res) {
  * @param {Object} res Response object
  * @returns {Object} Object with a list of user atlases
  */
-const apiUserAtlas = function(req, res) {
+const apiUserAtlas = async function (req, res) {
   // @todo Check access rights for this route
-  var {userName} = req.params;
-  let {start, length} = req.query;
+  var { userName } = req.params;
+  let { start, length } = req.query;
 
   console.log("userName:", userName, "start:", start, "length:", length);
-  if(typeof start === "undefined") {
-    res.status(403).send({error: "Provide 'start'"});
+  if (typeof start === "undefined") {
+    res.status(403).send({ error: "Provide 'start'" });
 
     return;
   }
-  if(typeof length === "undefined") {
-    res.status(403).send({error: "Provide 'length'"});
+  if (typeof length === "undefined") {
+    res.status(403).send({ error: "Provide 'length'" });
 
     return;
   }
   start = parseInt(start);
   length = parseInt(length);
 
-  dataSlices.getUserAtlasSlice(req, userName, start, length)
-    .then(function(result) {
-      res.send(result);
-    })
-    .catch(function(err) {
+  const result = await dataSlices.getUserAtlasSlice(req, userName, start, length)
+    .catch(function (err) {
       console.log("ERROR:", err);
-      res.send({success:false, list:[]});
+      res.send({ success: false, list: [] });
     });
+  res.send(result);
 };
 
 /**
@@ -170,36 +160,34 @@ const apiUserAtlas = function(req, res) {
  * @param {Object} res Response object
  * @returns {Object} Object with a list of user projects
  */
-const apiUserProjects = function(req, res) {
+const apiUserProjects = async function (req, res) {
   // @todo Check access rights for this route
-  var {userName} = req.params;
-  let {start, length} = req.query;
+  var { userName } = req.params;
+  let { start, length } = req.query;
 
   console.log("userName:", userName, "start:", start, "length:", length);
-  if(typeof start === "undefined") {
-    res.status(403).send({error: "Provide 'start'"});
+  if (typeof start === "undefined") {
+    res.status(403).send({ error: "Provide 'start'" });
 
     return;
   }
-  if(typeof length === "undefined") {
-    res.status(403).send({error: "Provide 'length'"});
+  if (typeof length === "undefined") {
+    res.status(403).send({ error: "Provide 'length'" });
 
     return;
   }
   start = parseInt(start);
   length = parseInt(length);
 
-  dataSlices.getUserProjectsSlice(req, userName, start, length)
-    .then(function(result) {
-      res.send(result);
-    })
-    .catch(function(err) {
+  const result = await dataSlices.getUserProjectsSlice(req, userName, start, length)
+    .catch(function (err) {
       console.log("ERROR:", err);
-      res.send({success:false, list:[]});
+      res.send({ success: false, list: [] });
     });
+  res.send(result);
 };
 
-const UserController = function() {
+const UserController = function () {
   this.validator = validator;
   this.apiUser = apiUser;
   this.apiUserAll = apiUserAll;
