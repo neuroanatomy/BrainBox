7ba944bdf0d2fc0b2921fedd1c3fe3611ebfe58b
Denis Hovart <hello@denishovart.com>
2022-02-21 00:31:38 +0100

    Update settings page file loading

--- view/brainbox/src/pages/project-settings-page.js
+++ view/brainbox/src/pages/project-settings-page.js
@@ -1,12 +1,11 @@
 /* global project, loggedUser */
 
 import 'nwl-components/dist/style.css';
-import { createApp, h, reactive } from 'vue';
+import { createApp, ref } from 'vue';
 import { SettingsPage } from 'nwl-components';
 import config from '../nwl-components-config';
 
 let cursorFiles = 0;
-
 const queryFiles = async (files) => {
   const res = await fetch(`/project/json/${project.shortname}/files?start=${cursorFiles}&length=100&name=true`);
   const list = await res.json();
@@ -22,17 +21,29 @@ const queryFiles = async (files) => {
   return files;
 };
 
-const props = reactive({ project });
-const app = createApp({
-  render () {
-    return h(SettingsPage, props);
+const PageContents = {
+  template: `
+    <SettingsPage :project="project" :files="files" />
+  `,
+  setup() {
+    const files = ref([]);
+
+    return {
+      project,
+      files
+    };
+  },
+  mounted() {
+    const fetchFiles = async () => {
+      const files = await queryFiles([]);
+      this.files.push(...files);
+    };
+    fetchFiles();
   }
-});
+};
+const app = createApp(PageContents);
+app.component('SettingsPage', SettingsPage);
 app.provide('config', config);
 app.provide('user', loggedUser);
+
 app.mount('#app');
-
-queryFiles([]).then((files) => {
-  props.project.files.list.push(...files);
-});
-
