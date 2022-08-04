ca3ebec6071ec7bf1aec36b250297677288a60cb
nicolas <ntraut9@gmail.com>
2022-08-04 17:38:48 +0200

    test websockets directly on brainbox

--- view/brainbox/src/pages/index-page.js
+++ view/brainbox/src/pages/index-page.js
@@ -2,107 +2,115 @@
 import '../style/style.css';
 import '../style/index-style.css';
 import '../style/ui.css';
+import Config from './../../../../cfg.json';
 
 /**
  * @function goToURL
  * @returns {void}
  */
-function goToURL() {
-  const url = document.getElementById("url").value;
-  location.href = "/mri?url=" + url;
-}
+const goToURL = function () {
+  const url = document.getElementById('url').value;
+  location.href = '/mri?url=' + url;
+};
 
 /**
  * @function testWebSockets
  * @returns {void}
  */
-function testWebSockets() {
-  console.log("testWebSockets");
+const testWebSockets = function () {
+  console.log('testWebSockets');
 
   return new Promise(function (resolve, reject) {
     // var host = "wss://websocketstest.com/service";
-    var host = "wss://echo.websocket.org";
-    var ws;
+    // var host = "wss://echo.websocket.org";
+    let host;
+    if(Config.secure) {
+      host = 'wss://' + Config.wshostname;
+    } else {
+      host = 'ws://' + Config.wshostname;
+    }
 
+    let ws;
     if (window.WebSocket) {
       ws = new window.WebSocket(host);
     } else if (window.MozWebSocket) {
       ws = new MozWebSocket(host);
     } else {
-      reject(new Error("BrainBox requires access to WebSockets, but this web browser does not support them. Try Firefox, Chrome or Safari."));
+      reject(new Error('BrainBox requires access to WebSockets, but this web browser does not support them. Try Firefox, Chrome or Safari.'));
     }
     ws.onopen = function () {
       ws.close();
-      resolve("Connection ok");
+      resolve('Connection ok');
     };
     ws.onerror = function (err) {
       console.log(err);
-      reject(new Error("BrainBox requires access to WebSockets, but your connection does not allow it. Ask your provider to allow WebSockets on port 8080"));
+      reject(new Error('BrainBox requires access to WebSockets, but your connection does not allow it. Ask your provider to allow WebSockets on port 8080'));
     };
   });
-}
+};
 
 testWebSockets()
   .then(function () {
-    console.log("Connection to websockets is ok");
+    console.log('Connection to websockets is ok');
   })
   .catch(function (m) {
+    // eslint-disable-next-line no-alert
     alert(m);
   });
 
-for (const slide of Array.from(document.getElementsByClassName("slide"))) {
+for (const slide of Array.from(document.getElementsByClassName('slide'))) {
   slide.style.height = window.innerHeight + 'px';
 }
 
 window.addEventListener('resize', () => {
-  for (const slide of document.getElementsByClassName("slide")) {
+  for (const slide of document.getElementsByClassName('slide')) {
     slide.style.height = window.innerHeight + 'px';
   }
 });
 
 // go to url button
-document.getElementById("go").addEventListener('click', goToURL);
+document.getElementById('go').addEventListener('click', goToURL);
 
 // video settings
-var vid = document.getElementById("bgBrains");
+var vid = document.getElementById('bgBrains');
 vid.onplaying = function () {
   vid.playbackRate = 0.5;
 };
 
 // List of brains
-document.getElementById("list").addEventListener("change", () => {
-  document.getElementById("url").value = document.getElementById("list").value;
+document.getElementById('list').addEventListener('change', () => {
+  document.getElementById('url').value = document.getElementById('list').value;
 });
 
-document.querySelector("h2").style.marginLeft = 0;
-document.querySelector("h2").style.opacity = 1;
+document.querySelector('h2').style.marginLeft = 0;
+document.querySelector('h2').style.opacity = 1;
 
 // Add URL loading
-document.getElementById("url").addEventListener("keyup", (e) => {
+document.getElementById('url').addEventListener('keyup', (e) => {
   if (e.keyCode === 13) {
     goToURL(e);
   }
 });
 
 // Connect addProject button
-document.getElementById("addProject").addEventListener("click", () => {
-  location.href = "/project/new";
+document.getElementById('addProject').addEventListener('click', () => {
+  location.href = '/project/new';
 });
 
 // multiple div as slides
 var menuShowing = true;
-window.addEventListener("scroll", () => {
+window.addEventListener('scroll', () => {
   const y = window.pageYOffset;
   if (y > 100 && menuShowing) {
-    document.getElementById("menu").style.top = -32;
-    document.getElementById("menu").style.opacity = 0;
+    document.getElementById('menu').style.top = -32;
+    document.getElementById('menu').style.opacity = 0;
     menuShowing = false;
 
-    document.getElementById("footer").style.display = "";
+    document.getElementById('footer').style.display = '';
   }
   if (y < 100 && !menuShowing) {
-    document.getElementById("menu").style.top = 0;
-    document.getElementById("menu").style.opacity = 1;
+    document.getElementById('menu').style.top = 0;
+    document.getElementById('menu').style.opacity = 1;
     menuShowing = true;
   }
 });
