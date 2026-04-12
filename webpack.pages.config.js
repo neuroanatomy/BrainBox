e9591ecbaf6fc05cf44e3e1258adc4deab20652f
Roberto Toro <roberto.toro@gmail.com>
2026-04-12 16:38:45 +0200

    add source maps

--- webpack.pages.config.js
+++ webpack.pages.config.js
@@ -20,6 +20,7 @@ module.exports = (env, argv) => ({
   optimization: {
     minimizer: [
       new TerserPlugin({
+        sourceMap: true,
         terserOptions: {
           mangle: false
         }
